019d5119a4c7e775fa482f497573d848a2377bb6
Hemi Luan <hemi.luan@gmail.com>
2016-12-02 00:47:46 +0000

    H_luan
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_4/madman/Rpacks/statTarget@124722 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/statTarget-package.R
+++ R/statTarget-package.R
@@ -2,13 +2,13 @@
 #' @aliases statTarget-package
 #' @docType package
 #' @title Statistical Analysis of Metabolite Profile
-#' @description An easy to use tool provide graphical user interface 
+#' @description An easy to use tool provides graphical user interface 
 #' for quality control based signal correction, integration of 
 #' metabolomic data from multiple batches, and the 
 #' comprehensive statistic analysis for non-targeted and targeted approaches.
 #' @details Package: statTarget
 #' @details Type: package
-#' @details Version: 1.4.3
+#' @details Version: 1.4.5
 #' @details Date: 2016-07-014
 #' @details License: GPL (>= 2)
 #' @author Hemi Luan
