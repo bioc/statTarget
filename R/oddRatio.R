861725cef4bc41b39702b70035fc40abf78ff2e5
Hemi Luan <hemi.luan@gmail.com>
2016-12-15 14:44:42 +0000

    H_luan
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_4/madman/Rpacks/statTarget@125212 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/oddRatio.R
+++ R/oddRatio.R
@@ -61,7 +61,7 @@ oddRatio <- function(file) {
         IJM <- as.matrix(IJ[,2:ncol(IJ)])
         outFactor <-as.factor(IJ[,1])
         logit_or <- function(outcome,a){
-                    glm.log <- glm(outcome~scale(a),
+                    glm.log <- glm(outcome~scale(a),control = list(maxit = 50),
                                    family=binomial(link="logit"))
                     res <- summary(glm.log)$coefficients[2,1:2]
                     p <- summary(glm.log)$coefficients[2,4]
